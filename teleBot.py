7782730c45c2e8823fc6c01b22fa698337da090f
Izlaster <izmaylovle0@gmail.com>
2023-04-10 02:03:01 +0300

    changed lem

--- teleBot.py
+++ teleBot.py
@@ -5,12 +5,11 @@ import telebot
 import difflib
 
 import pandas as pd
-from conllu import parse
-from nltk import DependencyGraph
-from ufal.udpipe import Model, Pipeline
 import re
-from transformers import AutoTokenizer
-import numpy as np
+from transformers import BertTokenizer
+
+from pymorphy2 import MorphAnalyzer
+from nltk.corpus import stopwords
 import random
 
 from modeling import *
@@ -22,11 +21,10 @@ logging.basicConfig(level=logging.INFO)
 bot = telebot.TeleBot(config.TOKEN)
 
 # import main df
-df = pd.read_excel('make_dataset/08042023_dataset_sort_change_tokenizer_fix.xlsx')
+df = pd.read_excel('make_dataset/10042023_dataset_sort_change_tokenizer_fix_fix.xlsx')
 
 # tokenizer + model
 tokenizer = BertTokenizer.from_pretrained(pre_trained_model_ckpt)
-model = Model.load('model/russian-ud-2.0-170801.udpipe')
 class_names = ['поступление - перевод', 'общежитие', 'учебная деятельность', 'внеучебная деятельность', 'документы', 'работа', 'финансы']
 myModel = SentimentClassifier(len(class_names))
 myModel.load_state_dict(torch.load('model/best_model_state.bin'))
@@ -37,86 +35,20 @@ def mySort(s1, s2):
   matcher = difflib.SequenceMatcher(None, s1, s2)
   return matcher.ratio()
 
-# subject object verb
-def get_sov(sent):
-    graph = DependencyGraph(tree_str=sent)
-    sov = {}
-    for triple in graph.triples():
-        if triple:
-            if triple[0][1] == 'VERB':
-                sov[triple[0][0]] = {'subj':'','obj':''}
-    for triple in graph.triples():
-        if triple:
-            if triple[1] == 'nsubj':
-                if triple[0][1] == 'VERB':
-                    sov[triple[0][0]]['subj']  = triple[2][0]
-            if 'obj' in triple[1]:
-                if triple[0][1] == 'VERB':
-                    sov[triple[0][0]]['obj'] = triple[2][0]
-    return sov
+patterns = "[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+"
+stopwords_ru = stopwords.words("russian")
+morph = MorphAnalyzer()
 
-# token function
-def tokenText(text):
-    arrParsed = []
-    longParsed = ''
-
-    sent = str(text)
-    sent = re.sub("\s\s+", ' ', sent)
-    sent = sent.lower()
-    sent = sent.strip()
-    sent = sent.replace(u'\xa0', u' ')
-    sentS = re.split(";|! |\?|\. ", sent)
-    sentS = list(filter(None, sentS))
-
-    for nSentS in sentS:
-        nSentS = re.sub(r'[^\w\s]','', nSentS) 
-
-        pipeline = Pipeline(model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT, Pipeline.DEFAULT)
-        parsed = pipeline.process(nSentS)
-
-        # костыли для dependency graph
-        parsed = '\n'.join([line for line in parsed.split('\n') if not line.startswith('#')])
-        parsed = parsed.replace('\troot\t', '\tROOT\t')
-
-        if (len(longParsed) < len(parsed)):
-            longParsed = parsed
-
-        arrParsed.append(parsed)
-    arrParsed = list(filter(None, arrParsed))
-
-    arrSov = []
-    for nArrParsed in arrParsed:
-        sov = get_sov(nArrParsed)
-        arrSov.append(sov)
-
-    bigSov = arrSov[0]
-    if (len(arrSov) > 1):
-        for i in range(1, len(arrSov)):
-            bigSov = { **bigSov, ** arrSov[i] }
-
-    tokenizedA = []
-    if (bigSov == {}):
-        arrGraph = []
-        graph = DependencyGraph(tree_str=longParsed)
-        for i in range(0, len(list(graph.triples()))):
-            for j in range(0, len(list(graph.triples())[0])):
-                if (list(graph.triples())[i][j][1] == 'NOUN'):
-                    arrGraph.append(list(graph.triples())[i][j][0])
-        arrGraph = set(arrGraph)
-        if (len(arrGraph) == 0):
-            tokenizedA.append('!');
-        else:
-            for nArrGraph in arrGraph:
-                tokenizedA.append(tokenizer.encode(nArrGraph, add_special_tokens=False))
-            tokenizedA = np.concatenate(tokenizedA, axis=0, out=None, dtype=None, casting="same_kind")
-    else:
-        for k, v in bigSov.items(): 
-            tokenizedA.append(tokenizer.encode(k, add_special_tokens=False))
-            tokenizedA.append(tokenizer.encode(v['subj'], add_special_tokens=False))
-            tokenizedA.append(tokenizer.encode(v['obj'], add_special_tokens=False))
-        tokenizedA = np.concatenate(tokenizedA, axis=0, out=None, dtype=None, casting="same_kind")
-
-    return tokenizedA
+def lemmatize(doc):
+    doc = re.sub(patterns, ' ', doc)
+    tokens = []
+    for token in doc.split():
+        if token and token not in stopwords_ru:
+            token = token.strip()
+            token = morph.normal_forms(token)[0]
+            
+            tokens.append(token)
+    return tokenizer.encode(" ".join(tokens), add_special_tokens=False)
 
 
 @bot.message_handler(commands=['start'])
@@ -135,7 +67,7 @@ def lalala(message):
     output = myModel(input_ids, attention_mask)
     _,prediction = torch.max(output, dim=1)
 
-    tToken = tokenText(text)
+    tToken = lemmatize(text)
     tq = []
     index = 0
     probability = 0.00
@@ -144,12 +76,12 @@ def lalala(message):
     questions = []
 
     for i in range(0, len(df)):
-        if (probability <= mySort(tToken, eval(df['tq'][i])) and df['label'][i] == int(prediction)):
-            tq = eval(df['tq'][i])
+        if (probability <= mySort(tToken, eval(str(df['tq_fix'][i]))) and df['label'][i] == int(prediction)):
+            tq = eval(str(df['tq_fix'][i]))
             index = i
             textQ = df['q'][i]
             textA = df['a'][i]
-            probability = mySort(tToken, eval(df['tq'][i]))
+            probability = mySort(tToken, eval(str(df['tq_fix'][i])))
             questions.append([tq, index, textQ, textA, probability])
 
     max_last_elem = max([que[-1] for que in questions])
